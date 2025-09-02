a25928f353aef21c3127bd924e55342ff5c8ff01
MashitahSahar <mashitahsahar@gmail.com>
2025-09-02 11:11:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import streamlit as st
 
 # Title of the app
-st.title("My First Streamlit App")
+st.title("Finance Data")
 
-# Text input
-name = st.text_input("Enter your name:")
-
-# Number input
-age = st.number_input("Enter your age:", min_value=1, max_value=120, step=1)
-
-# Button
-if st.button("Submit"):
-    st.write(f"Hello, {name}! 👋")
-    st.write(f"You are {age} years old.")
 
 import streamlit as st
 import pandas as pd
